f395e8327685d6d7ba5a2c077d47362635c92feb
Karen Huang <huangkaiyun1@gmail.com>
2022-08-17 14:43:26 +0800

    add rwd

--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
   isCollapsed = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  resetCollapsed() {
+    if (window.innerWidth <= 768) this.isCollapsed = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.resetCollapsed();
+  }
+
+  ngOnInit(): void {
+    this.resetCollapsed();
+  }
 }
